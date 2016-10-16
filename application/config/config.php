8d65e36ebf863248fa09575671d38800b57e7ffd
Puncoz Nepal <mail@pankajnepal.com.np>
2016-10-16 14:22:26 +0545

    baseurl for heroku

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+echo $_SERVER['SERVER_NAME'];exit;
 $config['base_url'] = '';
 
 /*
